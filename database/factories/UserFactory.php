0df0ed0934170dbbb90319fb927a730d69edcd73
wizard07 <yonatanfrans07@gmail.com>
2024-07-13 06:29:43 +0700

    first commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'username' => 'yonathanfrans',
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('wizard07'),
             'remember_token' => Str::random(10),
         ];
     }
