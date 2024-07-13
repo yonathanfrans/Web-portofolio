d2258575973fc28aca0d81e72908f8a916f63412
wizard07 <yonatanfrans07@gmail.com>
2024-07-13 20:05:21 +0700

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./set_permissions.sh && php -S 9000 -t public
+web: ./set_permissions.sh && php -S 0.0.0.0:9000 -t public
--- set_permissions.sh
+++ set_permissions.sh
@@ -4,3 +4,5 @@
 chmod -R 775 /app/public/assets/file
 chown -R www-data:www-data /app/public/assets/file
 
+chmod -R 775 /app/public/assets/img
+chown -R www-data:www-data /app/public/assets/img
